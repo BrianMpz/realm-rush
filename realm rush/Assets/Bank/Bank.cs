262ab42cb56749e688c294565b43aeca379a1652
BrianMpz <119757141+BrianMpz@users.noreply.github.com>
2023-02-05 18:18:49 +0000

    Initial commit

---
+++ realm rush/Assets/Bank/Bank.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Bank : MonoBehaviour
+{
+    [SerializeField] int startingBalance = 150;
+    
+    [SerializeField] int currentBalance;
+    public int CurrentBalance { get { return currentBalance; } }
+
+    [SerializeField] TextMeshProUGUI balanceScreenp;
+
+    void Update()
+    {
+        UpdateDisplay();
+    }
+    void Awake()
+    {
+        currentBalance = startingBalance;
+        UpdateDisplay();
+    }
+
+    public void Deposit(int amount)
+    {
+        UpdateDisplay();
+        currentBalance += Mathf.Abs(amount);
+    }
+
+    public void Withdraw(int amount)
+    {
+        UpdateDisplay();
+        currentBalance -= Mathf.Abs(amount);
+        if(currentBalance < 0)
+        {
+            //Lose the game;
+            ReloadScene();
+            
+        }
+    }
+
+    void UpdateDisplay()
+    {
+        balanceScreenp.text = "Gold: " + currentBalance;
+    }
+
+    void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+}
---
+++ realm rush/Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float maxHitPoints = 5;
+    [SerializeField] float DiffRamp = 1;
+    [SerializeField] float currentHitPoints = 0;
+
+    Enemy enemy;
+
+    void OnEnable()
+    {
+        currentHitPoints = maxHitPoints;        
+    }
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    void OnParticleCollision(GameObject other) 
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit() 
+    {
+        currentHitPoints--;
+
+        if(currentHitPoints <= 0)
+        {
+            maxHitPoints += DiffRamp;
+            gameObject.SetActive(false);
+            enemy.RewardGold();
+            maxHitPoints += DiffRamp;
+        }
+    }
+}
